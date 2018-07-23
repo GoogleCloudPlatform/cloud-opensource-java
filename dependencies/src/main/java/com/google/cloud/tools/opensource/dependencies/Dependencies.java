48476dabf1a984b21eac599a4014d24ab30a0793
Elliotte Harold <elharo@google.com>
2018-07-23 08:56:21 -0400

    begin converting to library

--- dependencies/src/main/java/com/google/cloud/tools/opensource/dependencies/Dependencies.java
+++ dependencies/src/main/java/com/google/cloud/tools/opensource/dependencies/Dependencies.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.cloud.tools.opensource.dependencies;
 
 import org.eclipse.aether.DefaultRepositorySystemSession;
@@ -14,6 +30,10 @@ import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.DependencyRequest;
 import org.eclipse.aether.resolution.DependencyResolutionException;
 import org.eclipse.aether.util.graph.visitor.PreorderNodeListGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
 import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
 import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
@@ -21,34 +41,14 @@ import org.eclipse.aether.spi.connector.transport.TransporterFactory;
 import org.eclipse.aether.transport.file.FileTransporterFactory;
 import org.eclipse.aether.transport.http.HttpTransporterFactory;
 
+/**
+ * Based on Apache Maven Artifact Resolver; formerly Eclipse Aether.
+ */
 public class Dependencies {
-  public static void main(String[] args) throws DependencyCollectionException, DependencyResolutionException {
   
-// Apache Maven Artifact Resolver
+  public static void main(String[] args) throws DependencyCollectionException, DependencyResolutionException {
 
-    RepositorySystem repoSystem = newRepositorySystem();
-    DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
-    // session.setDependencyTraverser(dependencyTraverser);
-
-    LocalRepository localRepo = new LocalRepository("target/local-repo");
-    session.setLocalRepositoryManager(repoSystem.newLocalRepositoryManager(session, localRepo));
-
-    Artifact artifact = new DefaultArtifact("com.google.cloud:google-cloud-bigquery:1.37.1");
-    Dependency dependency = new Dependency(artifact, "compile");
-    RemoteRepository central =
-        new RemoteRepository.Builder("central", "default", "http://repo1.maven.org/maven2/")
-            .build();
-
-    CollectRequest collectRequest = new CollectRequest();
-    collectRequest.setRoot(dependency);
-    collectRequest.addRepository(central);
-    org.eclipse.aether.graph.DependencyNode node =
-        repoSystem.collectDependencies(session, collectRequest).getRoot();
-
-    DependencyRequest dependencyRequest = new DependencyRequest();
-    dependencyRequest.setRoot(node);
-
-    repoSystem.resolveDependencies(session, dependencyRequest);
+    DependencyNode node = resolveNode("com.google.cloud", "google-cloud-bigquery", "1.37.1");
 
     PreorderNodeListGenerator nlg = new PreorderNodeListGenerator();
     node.accept(nlg);
@@ -59,6 +59,32 @@ public class Dependencies {
     }
   }
 
+  private static DependencyNode resolveNode(String groupId, String artifactId, String version)
+      throws DependencyCollectionException, DependencyResolutionException {
+    RepositorySystem repoSystem = newRepositorySystem();
+    DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
+
+    LocalRepository localRepo = new LocalRepository("target/local-repo");
+    session.setLocalRepositoryManager(repoSystem.newLocalRepositoryManager(session, localRepo));
+
+    Artifact artifact = new DefaultArtifact(groupId + ':' + artifactId + ':' + version);
+    Dependency dependency = new Dependency(artifact, "compile");
+    RemoteRepository central =
+        new RemoteRepository.Builder("central", "default", "http://repo1.maven.org/maven2/")
+            .build();
+
+    CollectRequest collectRequest = new CollectRequest();
+    collectRequest.setRoot(dependency);
+    collectRequest.addRepository(central);
+    DependencyNode node = repoSystem.collectDependencies(session, collectRequest).getRoot();
+
+    DependencyRequest dependencyRequest = new DependencyRequest();
+    dependencyRequest.setRoot(node);
+
+    repoSystem.resolveDependencies(session, dependencyRequest);
+    return node;
+  }
+
   private static RepositorySystem newRepositorySystem() {
     DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
     locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class);
@@ -66,5 +92,16 @@ public class Dependencies {
     locator.addService(TransporterFactory.class, HttpTransporterFactory.class);
 
     return locator.getService(RepositorySystem.class);
+  }
+
+  public static List<Artifact> getDependencies(String groupId, String artifactId, String version) 
+      throws DependencyCollectionException, DependencyResolutionException {
+    List<Artifact> result = new ArrayList<>();
+    
+    DependencyNode node = resolveNode(groupId, artifactId, version);
+    for (DependencyNode child : node.getChildren()) {
+      result.add(child.getArtifact());
+    }
+    return result;
   }
 }
