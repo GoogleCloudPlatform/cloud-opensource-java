0fdcbe7f50cb891ac1c45246fd691f844a1a2498
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2018-09-13 14:51:28 -0400

    Use Local Repository (#68)
    * refactor out some common maven functionality
    * use local repo when present

---
+++ dependencies/src/main/java/com/google/cloud/tools/opensource/dependencies/RepositoryUtility.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.opensource.dependencies;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.eclipse.aether.DefaultRepositorySystemSession;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
+import org.eclipse.aether.impl.DefaultServiceLocator;
+import org.eclipse.aether.repository.LocalRepository;
+import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
+import org.eclipse.aether.spi.connector.transport.TransporterFactory;
+import org.eclipse.aether.transport.file.FileTransporterFactory;
+import org.eclipse.aether.transport.http.HttpTransporterFactory;
+
+/**
+ * Aether initialization.
+ */
+final class RepositoryUtility {
+  
+  private RepositoryUtility() {}
+
+  static RepositorySystem newRepositorySystem() {
+    DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
+    locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class);
+    locator.addService(TransporterFactory.class, FileTransporterFactory.class);
+    locator.addService(TransporterFactory.class, HttpTransporterFactory.class);
+  
+    return locator.getService(RepositorySystem.class);
+  }
+
+  static RepositorySystemSession newSession(RepositorySystem system ) {
+    DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
+  
+    LocalRepository localRepository = new LocalRepository(findLocalRepository().getAbsolutePath());
+    session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepository));
+    session.setReadOnly();
+    
+    return session;
+  }
+
+  private static File findLocalRepository() {
+    Path home = Paths.get(System.getProperty("user.home"));
+    Path localRepo = home.resolve(".m2").resolve("repository");
+    if (Files.isDirectory(localRepo)) {
+      return localRepo.toFile();
+    } else {
+      File temporaryDirectory = com.google.common.io.Files.createTempDir();
+      temporaryDirectory.deleteOnExit();
+      return temporaryDirectory; 
+   }
+  }
+
+}
---
+++ dependencies/src/test/java/com/google/cloud/tools/opensource/dependencies/RepositoryUtilityTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.opensource.dependencies;
+
+import java.io.File;
+
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RepositoryUtilityTest {
+
+  @Test
+  public void testFindLocalRepository() {
+    RepositorySystem system = RepositoryUtility.newRepositorySystem();
+    RepositorySystemSession session = RepositoryUtility.newSession(system);
+    
+    File local = session.getLocalRepository().getBasedir();
+    Assert.assertTrue(local.exists());
+    Assert.assertTrue(local.canRead());
+    Assert.assertTrue(local.canWrite());
+  }
+  
+  
+}
