fbf17c408236d5d78ac8d2faed433a2f6600c709
suzu tomo <suztomo@gmail.com>
2018-10-02 11:58:53 -0400

    Added DependencyTreeFormatter for a tool for printing dependency tree (#105)
    * Added DependencyTreeFormatter for a tool for printing dependency tree #92
    
    See https://github.com/GoogleCloudPlatform/cloud-opensource-java/pull/105 for example invocation

---
+++ dependencies/src/main/java/com/google/cloud/tools/opensource/dependencies/DependencyTreeFormatter.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.opensource.dependencies;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Strings;
+import java.util.List;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.collection.DependencyCollectionException;
+import org.eclipse.aether.resolution.DependencyResolutionException;
+
+/**
+ * Formats and prints artifact dependency tree represented by list of {@link DependencyPath}
+ */
+class DependencyTreeFormatter {
+
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.err.println("Maven coordinate not provided. E.g., 'io.grpc:grpc-auth:1.15.0'");
+      return;
+    }
+    for (String coordinate : args) {
+      try {
+        printDependencyTree(coordinate);
+      } catch (DependencyCollectionException | DependencyResolutionException e) {
+        System.err.println(coordinate + " : Failed to retrieve dependency information:"
+            + e.getMessage());
+      }
+    }
+  }
+
+  /**
+   * Prints dependencies for the coordinate of an artifact
+   *
+   * @param coordinate Maven coordinate of an artifact to print its dependencies
+   * @throws DependencyCollectionException when dependencies cannot be collected
+   * @throws DependencyResolutionException when dependencies cannot be resolved
+   */
+  private static void printDependencyTree(String coordinate)
+      throws DependencyCollectionException, DependencyResolutionException {
+    DefaultArtifact rootArtifact = new DefaultArtifact(coordinate);
+    DependencyGraph dependencyGraph = DependencyGraphBuilder.getCompleteDependencies(rootArtifact);
+    System.out.println("Dependencies for " + coordinate);
+    System.out.println(formatDependencyPaths(dependencyGraph.list()));
+  }
+
+  /**
+   * Prints dependencies expressed in dependency paths in tree in similar way to mvn dependency:tree
+   *
+   * @param dependencyPaths sorted dependency paths
+   */
+  @VisibleForTesting
+  static String formatDependencyPaths(List<DependencyPath> dependencyPaths) {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (DependencyPath dependencyPath : dependencyPaths) {
+      int depth = dependencyPath.size();
+      String indentCharacter = "  ";
+      stringBuilder.append(Strings.repeat(indentCharacter, depth) + dependencyPath.getLeaf()+"\n");
+    }
+    return stringBuilder.toString();
+  }
+}
---
+++ dependencies/src/test/java/com/google/cloud/tools/opensource/dependencies/DependencyTreeFormatterTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.opensource.dependencies;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DependencyTreeFormatterTest {
+
+  @Test
+  public void testDependencyTree() {
+    List<DependencyPath> dependencyPathList = new ArrayList<>();
+
+    // 4 artifacts as DependencyPath dummy inputs
+    DependencyPath dependency1 = new DependencyPath();
+    dependency1.add(new DefaultArtifact("io.grpc:grpc-auth:jar:1.15.0"));
+    dependencyPathList.add(dependency1);
+
+    DependencyPath dependency2 = new DependencyPath();
+    dependency2.add(new DefaultArtifact("io.grpc:grpc-auth:jar:1.15.0"));
+    dependency2.add(new DefaultArtifact("io.grpc:grpc-core:jar:1.15.0"));
+    dependencyPathList.add(dependency2);
+
+    DependencyPath dependency3 = new DependencyPath();
+    dependency3.add(new DefaultArtifact("io.grpc:grpc-auth:jar:1.15.0"));
+    dependency3.add(new DefaultArtifact("io.grpc:grpc-core:jar:1.15.0"));
+    dependency3.add(new DefaultArtifact("io.grpc:grpc-context:jar:1.15.0"));
+    dependencyPathList.add(dependency3);
+
+    DependencyPath dependency4 = new DependencyPath();
+    dependency4.add(new DefaultArtifact("io.grpc:grpc-auth:jar:1.15.0"));
+    dependency4.add(new DefaultArtifact("io.grpc:grpc-core:jar:1.15.0"));
+    dependency4.add(new DefaultArtifact("com.google.code.gson:gson:jar:2.7"));
+    dependencyPathList.add(dependency4);
+
+    String actualTreeOutput = DependencyTreeFormatter.formatDependencyPaths(dependencyPathList);
+    String expectedTreeOutput =
+        "  io.grpc:grpc-auth:jar:1.15.0\n"
+            + "    io.grpc:grpc-core:jar:1.15.0\n"
+            + "      io.grpc:grpc-context:jar:1.15.0\n"
+            + "      com.google.code.gson:gson:jar:2.7\n";
+    Assert.assertEquals(
+        "The dependency should be output as tree with indentation",
+        expectedTreeOutput, actualTreeOutput);
+  }
+}
