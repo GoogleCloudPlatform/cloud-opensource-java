9b30d692ac2a02703171b21c78d4bdb2a5d4a5a5
Tomo Suzuki <suztomo@google.com>
2018-12-20 11:11:02 -0500

    Added files

--- dependencies/src/main/java/com/google/cloud/tools/opensource/classpath/LinkageErrorOnReference.java
+++ dependencies/src/main/java/com/google/cloud/tools/opensource/classpath/LinkageErrorOnReference.java
@@ -1,5 +1,120 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.cloud.tools.opensource.classpath;
 
-public class LinkageErrorOnReference {
+import com.google.auto.value.AutoValue;
+import java.net.URL;
+import javax.annotation.Nullable;
 
+/**
+ * Static linkage error caused by a symbol reference. The symbol reference is one of the three
+ * types: {@link ClassSymbolReference}, {@link MethodSymbolReference}, and {@link
+ * FieldSymbolReference}.
+ *
+ * @param <T> type of symbol reference that causes the linkage error
+ * @see <a href="https://github.com/GoogleCloudPlatform/cloud-opensource-java/blob/master/library-best-practices/glossary.md#static-linkage-error">
+ * Java Dependency Glossary: Static linkage error</a>
+ */
+@AutoValue
+abstract class LinkageErrorOnReference<T extends SymbolReference> {
+
+  /**
+   * Returns a symbol reference that caused this linkage error.
+   */
+  abstract T getReference();
+
+  /**
+   * Returns the location of the target class in a symbol reference; null if the target class is not
+   * found in the class path or the source location is unavailable.
+   */
+  @Nullable
+  abstract URL getTargetClassLocation();
+
+  /**
+   * Returns the reason why the symbol reference is marked as a linkage error.
+   */
+  abstract Reason getReason();
+
+  /**
+   * Returns a linkage error caused by {@link Reason#TARGET_CLASS_NOT_FOUND}.
+   */
+  static <U extends SymbolReference> LinkageErrorOnReference<U> errorMissingTargetClass(
+      U reference) {
+    return builderFor(reference).setReason(Reason.TARGET_CLASS_NOT_FOUND).build();
+  }
+
+  /**
+   * Returns a linkage error caused by {@link Reason#MISSING_MEMBER}.
+   */
+  static <U extends SymbolReference> LinkageErrorOnReference<U> errorMissingMember(
+      U reference, URL targetClassLocation) {
+    return builderFor(reference).setReason(Reason.MISSING_MEMBER)
+        .setTargetClassLocation(targetClassLocation).build();
+  }
+
+  /**
+   * Returns a linkage error caused by {@link Reason#INVALID_ACCESS_MODIFIER}.
+   */
+  static <U extends SymbolReference> LinkageErrorOnReference<U> errorInvalidModifier(
+      U reference, URL targetClassLocation) {
+    return builderFor(reference).setReason(Reason.INVALID_ACCESS_MODIFIER)
+        .setTargetClassLocation(targetClassLocation).build();
+  }
+
+  /**
+   * Returns {@code Builder} for a linkage error caused by the symbol reference.
+   */
+  private static <U extends SymbolReference> Builder<U> builderFor(U reference) {
+    Builder<U> builder = new AutoValue_LinkageErrorOnReference.Builder<>();
+    builder.setReference(reference);
+    return builder;
+  }
+
+  @AutoValue.Builder
+  abstract static class Builder<T extends SymbolReference> {
+
+    abstract LinkageErrorOnReference.Builder<T> setTargetClassLocation(URL targetClassLocation);
+
+    abstract LinkageErrorOnReference.Builder<T> setReason(Reason reason);
+
+    abstract LinkageErrorOnReference.Builder<T> setReference(T reference);
+
+    abstract LinkageErrorOnReference<T> build();
+  }
+
+  /**
+   * Reason to distinguish the cause of a static linkage error against a symbol reference.
+   */
+  enum Reason {
+    /**
+     * The target class of the symbol reference is not found in the class path.
+     */
+    TARGET_CLASS_NOT_FOUND,
+
+    /**
+     * The access modifier (e.g., public or protected) does not allow the source of the symbol
+     * reference to use the target symbol.
+     */
+    INVALID_ACCESS_MODIFIER,
+
+    /**
+     * For a method or field reference, the member is not found in the target class in the class
+     * path.
+     */
+    MISSING_MEMBER
+  }
 }
--- dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/LinkageErrorOnReferenceTest.java
+++ dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/LinkageErrorOnReferenceTest.java
@@ -1,5 +1,60 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.cloud.tools.opensource.classpath;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.junit.Test;
+
 public class LinkageErrorOnReferenceTest {
+
+  @Test
+  public void testLinkageErrorCreation() throws MalformedURLException {
+    FieldSymbolReference fieldSymbolReference =
+        FieldSymbolReference.builder()
+            .setTargetClassName("ClassC")
+            .setFieldName("fieldX")
+            .setSourceClassName("ClassD")
+            .build();
+
+    LinkageErrorOnReference<FieldSymbolReference> fieldError = LinkageErrorOnReference
+        .errorMissingTargetClass(fieldSymbolReference);
+
+    MethodSymbolReference methodSymbolReference =
+        MethodSymbolReference.builder()
+            .setTargetClassName("ClassA")
+            .setMethodName("methodX")
+            .setDescriptor("java.lang.String")
+            .setSourceClassName("ClassB")
+            .build();
+
+    URL targetClassLocation = new URL("file://foo/bar");
+    LinkageErrorOnReference<MethodSymbolReference> methodError = LinkageErrorOnReference
+        .errorMissingMember(methodSymbolReference, targetClassLocation);
+
+
+    ClassSymbolReference classSymbolReference =
+        ClassSymbolReference.builder()
+            .setTargetClassName("ClassA")
+            .setSourceClassName("ClassB")
+            .build();
+    LinkageErrorOnReference<ClassSymbolReference> classError = LinkageErrorOnReference
+        .errorInvalidModifier(null, targetClassLocation);
+
+  }
 
 }
