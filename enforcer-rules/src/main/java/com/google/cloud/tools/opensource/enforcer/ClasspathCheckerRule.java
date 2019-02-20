d3b153e0e9feb53526a57dea122f5c8247389bcd
Tomo Suzuki <suztomo@google.com>
2019-02-20 11:18:20 -0500

    Exploring Maven environment

--- enforcer-rules/src/main/java/com/google/cloud/tools/opensource/enforcer/ClasspathCheckerRule.java
+++ enforcer-rules/src/main/java/com/google/cloud/tools/opensource/enforcer/ClasspathCheckerRule.java
@@ -16,31 +16,44 @@
 
 package com.google.cloud.tools.opensource.enforcer;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
 import com.google.cloud.tools.opensource.classpath.ClasspathCheckReport;
 import com.google.cloud.tools.opensource.classpath.ClasspathChecker;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.resolver.ArtifactCollector;
+import java.util.Objects;
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.resolver.ArtifactResolver;
-import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
 import org.apache.maven.enforcer.rule.api.EnforcerRule;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.execution.RuntimeInformation;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.shared.dependency.tree.DependencyNode;
-import org.apache.maven.shared.dependency.tree.DependencyTreeBuilder;
-import org.apache.maven.shared.dependency.tree.DependencyTreeBuilderException;
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.apache.maven.repository.legacy.resolver.DefaultLegacyArtifactCollector;
+import org.apache.maven.shared.dependency.graph.DependencyGraphBuilder;
+import org.apache.maven.shared.dependency.graph.DependencyGraphBuilderException;
+import org.apache.maven.shared.dependency.graph.DependencyNode;
+import org.apache.maven.shared.dependency.graph.traversal.CollectingDependencyNodeVisitor;
+import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.codehaus.plexus.context.Context;
+import org.eclipse.aether.DefaultRepositorySystemSession;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.collection.CollectRequest;
+import org.eclipse.aether.collection.DependencyCollectionException;
+import org.eclipse.aether.impl.DependencyCollector;
+import org.eclipse.aether.repository.ArtifactRepository;
 
 /**
  * Introduction to Maven Enforcer Plugin Custom Rule
@@ -53,29 +66,86 @@ public class ClasspathCheckerRule implements EnforcerRule {
   /** Simple param. This rule will fail if the value is true. */
   private boolean shouldIfail = false;
 
-  private DependencyNode getNode(EnforcerRuleHelper helper) throws EnforcerRuleException {
+  private DependencyNode getRootDependencyNode(MavenProject project, EnforcerRuleHelper helper)
+      throws EnforcerRuleException {
     try {
-      MavenProject project = (MavenProject) helper.evaluate("${project}");
-      DependencyTreeBuilder dependencyTreeBuilder =
-          helper.getComponent(DependencyTreeBuilder.class);
-      ArtifactRepository repository = (ArtifactRepository) helper.evaluate("${localRepository}");
-      ArtifactFactory factory = helper.getComponent(ArtifactFactory.class);
-      ArtifactMetadataSource metadataSource = helper.getComponent(ArtifactMetadataSource.class);
-      ArtifactCollector collector = helper.getComponent(ArtifactCollector.class);
-      ArtifactFilter filter = null; // we need to evaluate all scopes
-      DependencyNode node =
-          dependencyTreeBuilder.buildDependencyTree(
-              project, repository, factory, metadataSource, filter, collector);
-      return node;
-    } catch (ExpressionEvaluationException e) {
-      throw new EnforcerRuleException(
-          "Unable to lookup an expression " + e.getLocalizedMessage(), e);
+      /* This throws ComponentLookupException
+      RepositorySystemSession repositorySystemSession = helper.getComponent(RepositorySystemSession.class);
+      */
+      /* This only gives me 2 items */
+      List<Dependency> dependencies = project.getDependencies();
+      DependencyGraphBuilder dependencyGraphBuilder =
+          helper.getComponent(DependencyGraphBuilder.class);
+
+      /* With org.eclipse.aether.RepositorySystem, it gets DefaultRepositorySystem */
+      RepositorySystem repositorySystem = helper.getComponent(RepositorySystem.class);
+
+      DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
+
+      /* With org.apache.maven.repository.RepositorySystem, it gets LegacyRepositorySystem
+      RepositorySystem repositorySystem = helper.getComponent(RepositorySystem.class);*/
+      //      List list = helper.getComponentList(null);
+
+      /* No such thing
+      RepositorySystemSession repositorySystemSession =
+          helper.getComponent(RepositorySystemSession.class); */
+      DependencyCollector dependencyCollector = helper.getComponent(DependencyCollector.class);
+
+
+      /* No, this does not have Aether Artifact class
+      org.eclipse.aether.artifact.Artifact aetherArtifact =
+          helper.getComponent(org.eclipse.aether.artifact.Artifact.class);
+      CollectRequest collectRequest = new CollectRequest();
+      collectRequest.setRootArtifact(aetherArtifact); */
+
+      dependencyCollector.collectDependencies(session, null);
+
+      PlexusContainer container = helper.getContainer();
+      Context context = container.getContext();
+      DependencyNode dependencyNode = dependencyGraphBuilder.buildDependencyGraph(project, null);
+      ArtifactRepository artifactRepository = helper.getComponent(ArtifactRepository.class);
+
+      return dependencyNode;
+
+      /* The following is from DependencyConvergenceRule
+              DependencyTreeBuilder dependencyTreeBuilder =
+            helper.getComponent(DependencyTreeBuilder.class);
+
+        ArtifactRepository repository = (ArtifactRepository) helper.evaluate("${localRepository}");
+        ArtifactFactory factory = helper.getComponent(ArtifactFactory.class);
+        ArtifactMetadataSource metadataSource = helper.getComponent(ArtifactMetadataSource.class);
+        ArtifactCollector collector = helper.getComponent(ArtifactCollector.class);
+        ArtifactFilter filter = null; // we need to evaluate all scopes
+        DependencyNode node =
+            dependencyTreeBuilder.buildDependencyTree(
+                project, repository, factory, metadataSource, filter, collector);
+        return node;
+      } catch (ExpressionEvaluationException e) {
+        throw new EnforcerRuleException(
+            "Unable to lookup an expression " + e.getLocalizedMessage(), e);*/
     } catch (ComponentLookupException e) {
       throw new EnforcerRuleException("Unable to lookup a component " + e.getLocalizedMessage(), e);
-    } catch (DependencyTreeBuilderException e) {
-      throw new EnforcerRuleException(
-          "Could not build dependency tree " + e.getLocalizedMessage(), e);
+    } catch (DependencyCollectionException | DependencyGraphBuilderException e) {
+      throw new EnforcerRuleException("Unable to build a dependency graph", e);
     }
+  }
+
+  private ImmutableList<Path> fetchClasspath(DependencyNode rootDependencyNode) {
+    CollectingDependencyNodeVisitor visitor = new CollectingDependencyNodeVisitor();
+    rootDependencyNode.accept(visitor);
+    return visitor.getNodes().stream()
+        .map(
+            node -> {
+              Artifact artifact = node.getArtifact();
+              File file = artifact.getFile();
+              if (file == null) {
+                return null;
+              }
+
+              return file.toPath();
+            })
+        .filter(Objects::nonNull)
+        .collect(toImmutableList());
   }
 
   public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException {
@@ -93,21 +163,20 @@ public class ClasspathCheckerRule implements EnforcerRule {
       RuntimeInformation rti = helper.getComponent(RuntimeInformation.class);
 
       // Does this work with cloud-opensource-java's DependencyNode?
-      DependencyNode node = getNode(helper);
+      DependencyNode projectNode = getRootDependencyNode(project, helper);
+      ImmutableList<Path> classpath = fetchClasspath(projectNode);
 
       log.info("Retrieved Target Folder: " + target);
       log.info("Retrieved ArtifactId: " + artifactId);
       log.info("Retrieved Project: " + project);
       log.info("Retrieved Project Dependencies: " + project.getDependencies());
       log.info("Retrieved Dependency Class: " + project.getDependencies().get(0).getClass());
-      log.info("Retrieved Dependency Node: " + node);
+      log.info("Retrieved Dependency Node: " + projectNode);
       log.info("Retrieved RuntimeInfo: " + rti);
       log.info("Retrieved Session: " + session);
       log.info("Retrieved Resolver: " + resolver);
 
-      List<Path> artifactsInPom = ImmutableList.of();
-      List<Path> classpath = ImmutableList.of();
-      List<Path> artifactJarsInBom = classpath.subList(0, artifactsInPom.size());
+      List<Path> artifactJarsInBom = classpath.subList(0, project.getDependencies().size());
       ImmutableSet<Path> entryPoints = ImmutableSet.copyOf(artifactJarsInBom);
 
       try {
