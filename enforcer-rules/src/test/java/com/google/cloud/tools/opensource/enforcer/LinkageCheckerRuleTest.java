6fe52b573c6ef818ced0eb6aa57c53973d261d26
Tomo Suzuki <suztomo@google.com>
2019-02-22 12:54:03 -0500

    Failure message in test

--- enforcer-rules/src/test/java/com/google/cloud/tools/opensource/enforcer/LinkageCheckerRuleTest.java
+++ enforcer-rules/src/test/java/com/google/cloud/tools/opensource/enforcer/LinkageCheckerRuleTest.java
@@ -125,7 +125,8 @@ public class LinkageCheckerRuleTest {
       // This artifact is known to contain classes missing dependencies
       setupMockDependencyResolution("com.google.appengine:appengine-api-1.0-sdk:1.9.64");
       rule.execute(mockRuleHelper);
-      Assert.fail();
+      Assert.fail(
+          "The rule should raise an EnforcerRuleException for artifacts missing dependencies");
     } catch (EnforcerRuleException ex) {
       // pass
       Assert.assertEquals(
