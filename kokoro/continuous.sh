a2244962277a8e8712addd08ebe9a5ea0d0564f1
Tomo Suzuki <suztomo@google.com>
2019-02-27 16:50:30 -0500

    Removed irrelevant comments

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -7,8 +7,5 @@ set -x
 
 export M2_HOME=/usr/local/apache-maven
 
-# https://stackoverflow.com/questions/3459928/running-a-specific-maven-plugin-goal-from-the-command-line-in-a-sub-module-of-a/26448447#26448447
-# https://stackoverflow.com/questions/11091311/maven-execjava-goal-on-a-multi-module-project
-
 cd github/cloud-opensource-java
 mvn -B clean install
