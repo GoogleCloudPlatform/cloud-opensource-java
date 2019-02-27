1c70c23ccd34bd9bd1bc52e17b11632cf8dc3670
Tomo Suzuki <suztomo@google.com>
2019-02-27 15:55:50 -0500

    continuous build for mac

---
+++ kokoro/macos_external/continuous.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+# Display commands being run.
+set -x
+
+export M2_HOME=/usr/local/apache-maven
+
+# https://stackoverflow.com/questions/3459928/running-a-specific-maven-plugin-goal-from-the-command-line-in-a-sub-module-of-a/26448447#26448447
+# https://stackoverflow.com/questions/11091311/maven-execjava-goal-on-a-multi-module-project
+
+cd github/cloud-opensource-java
+mvn -B clean install
+
+cd dashboard
+mvn -B exec:java -Dexec.mainClass="com.google.cloud.tools.opensource.dashboard.DashboardMain"
