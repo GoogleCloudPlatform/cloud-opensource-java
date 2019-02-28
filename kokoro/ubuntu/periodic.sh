e76d1b2add0ec353ccc1e23486b1b59173f9fae6
Tomo Suzuki <suztomo@google.com>
2019-02-28 11:50:17 -0500

    Cleaning up periodic shell script

--- kokoro/ubuntu/periodic.sh
+++ kokoro/ubuntu/periodic.sh
@@ -5,14 +5,12 @@ set -e
 # Display commands being run.
 set -x
 
-
-
-# https://stackoverflow.com/questions/3459928/running-a-specific-maven-plugin-goal-from-the-command-line-in-a-sub-module-of-a/26448447#26448447
-# https://stackoverflow.com/questions/11091311/maven-execjava-goal-on-a-multi-module-project
-
 cd github/cloud-opensource-java
 # M2_HOME is not used since Maven 3.5.0 https://maven.apache.org/docs/3.5.0/release-notes.html
 mvn -B clean install
 
+# Running target of dashboard submodule
+# https://stackoverflow.com/questions/3459928/running-a-specific-maven-plugin-goal-from-the-command-line-in-a-sub-module-of-a/26448447#26448447
+# https://stackoverflow.com/questions/11091311/maven-execjava-goal-on-a-multi-module-project
 cd dashboard
 mvn -B exec:java -Dexec.mainClass="com.google.cloud.tools.opensource.dashboard.DashboardMain"
