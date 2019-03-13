d36d40382c0d2bf97cc428556c7a89c7d95b9040
Tomo Suzuki <suztomo@google.com>
2019-03-13 11:15:02 -0400

    empty commit

--- dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/ClassDumperTest.java
+++ dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/ClassDumperTest.java
@@ -345,7 +345,6 @@ public class ClassDumperTest {
 
       Assert.fail("It should throw VerifyException when it cannot find a class symbol reference");
     } catch (VerifyException ex) {
-      // pass
       Truth.assertThat(ex)
           .hasMessageThat()
           .isEqualTo(
