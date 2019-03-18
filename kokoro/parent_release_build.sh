e857ff660d31793c182d4730106b2896838621b2
Tomo Suzuki <suztomo@gmail.com>
2019-03-18 15:02:32 -0400

    Add release scripts (#513)
    * Added prepare_release.sh
    
    * Adding build scripts for 3 pipelines

---
+++ kokoro/parent_release_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+# Display commands to stderr.
+set -x
+
+cd github/cloud-opensource-java
+
+# Build the project (excluding boms) to ensure validity of parent pom
+# The artifact is unused in this parent-pom build.
+mvn -pl 'dependencies,enforcer-rules' -Prelease -B -U verify
+
+# copy pom with the name expected in the Maven repository
+ARTIFACT_ID=$(mvn -B help:evaluate -Dexpression=project.artifactId 2>/dev/null | grep -v "^\[")
+PROJECT_VERSION=$(mvn -B help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\[")
+# This build is only for parent pom. Target directory is not automatically created.
+mkdir -p target
+cp pom.xml target/${ARTIFACT_ID}-${PROJECT_VERSION}.pom
