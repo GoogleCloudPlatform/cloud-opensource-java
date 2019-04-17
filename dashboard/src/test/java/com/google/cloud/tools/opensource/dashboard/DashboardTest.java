c52df2bc48be969d6a1a1d82723f606fa38a83a8
IRuizM <13287486+IRuizM@users.noreply.github.com>
2019-04-17 22:54:21 +0200

    Replaced deprecated Correspondence constructors (#570)
    * Replaced deprecated Correspondence constructors

--- dashboard/src/test/java/com/google/cloud/tools/opensource/dashboard/DashboardTest.java
+++ dashboard/src/test/java/com/google/cloud/tools/opensource/dashboard/DashboardTest.java
@@ -63,18 +63,9 @@ public class DashboardTest {
       Paths.get("..", "boms", "cloud-oss-bom", "pom.xml").toAbsolutePath();
 
   private static final Correspondence<Node, String> NODE_VALUES =
-      new Correspondence<Node, String>() {
-        @Override
-        public boolean compare(Node node, String expected) {
-          String trimmedValue = trimAndCollapseWhiteSpace(node.getValue());
-          return trimmedValue.equals(expected);
-        }
-
-        @Override
-        public String toString() {
-          return "has value equal to";
-        }
-      };
+      Correspondence.from((node, expected) ->
+          trimAndCollapseWhiteSpace(node.getValue())
+          .equals(expected), "has value equal to");
 
   private static String trimAndCollapseWhiteSpace(String value) {
     return CharMatcher.whitespace().trimAndCollapseFrom(value, ' ');
