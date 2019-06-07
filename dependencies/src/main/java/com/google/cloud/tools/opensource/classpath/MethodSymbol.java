107bb6892c0cb17e6b95381cb897df739362897b
Elliotte Rusty Harold <elharo@google.com>
2019-06-07 08:58:24 -0400

    Java 8 JLS URL

--- dependencies/src/main/java/com/google/cloud/tools/opensource/classpath/MethodSymbol.java
+++ dependencies/src/main/java/com/google/cloud/tools/opensource/classpath/MethodSymbol.java
@@ -81,7 +81,7 @@ public final class MethodSymbol extends Symbol {
 
   @Override
   public String toString() {
-    // https://docs.oracle.com/javase/specs/jls/se7/html/jls-8.html#jls-8.4.2
+    // https://docs.oracle.com/javase/specs/jls/se8/html/jls-8.html#jls-8.4.2
     String signaturePlusReturnType = Utility.methodSignatureToString(descriptor, name, "");
     String signature = signaturePlusReturnType.substring(signaturePlusReturnType.indexOf(' ') + 1);
     return (isInterfaceMethod ? "Interface " : "") + getClassName() + "'s method " + signature;
