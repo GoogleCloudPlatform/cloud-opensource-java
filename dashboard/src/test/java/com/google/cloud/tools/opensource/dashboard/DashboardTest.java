f11da95c2546b9b76028e0b9c4caa8c39eec1bf3
Tomo Suzuki <suztomo@google.com>
2019-06-11 17:08:53 -0400

    path

--- dashboard/src/test/java/com/google/cloud/tools/opensource/dashboard/DashboardTest.java
+++ dashboard/src/test/java/com/google/cloud/tools/opensource/dashboard/DashboardTest.java
@@ -24,6 +24,7 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
