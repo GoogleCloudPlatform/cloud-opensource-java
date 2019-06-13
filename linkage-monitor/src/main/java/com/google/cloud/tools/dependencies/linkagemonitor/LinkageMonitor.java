a40aee88938d9614f978c5523178203f17d2e040
Tomo Suzuki <suztomo@google.com>
2019-06-13 17:07:46 -0400

    LinkageMonitor to find latest BOM version

--- linkage-monitor/src/main/java/com/google/cloud/tools/dependencies/linkagemonitor/LinkageMonitor.java
+++ linkage-monitor/src/main/java/com/google/cloud/tools/dependencies/linkagemonitor/LinkageMonitor.java
@@ -18,20 +18,24 @@ package com.google.cloud.tools.dependencies.linkagemonitor;
 
 import com.google.cloud.tools.opensource.classpath.LinkageChecker;
 import com.google.cloud.tools.opensource.classpath.SymbolProblem;
+import com.google.cloud.tools.opensource.dependencies.Artifacts;
 import com.google.cloud.tools.opensource.dependencies.Bom;
 import com.google.cloud.tools.opensource.dependencies.RepositoryUtility;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Splitter;
 import com.google.common.base.Verify;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import org.eclipse.aether.RepositoryException;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.resolution.VersionRangeRequest;
 import org.eclipse.aether.resolution.VersionRangeResolutionException;
 import org.eclipse.aether.resolution.VersionRangeResult;
@@ -47,18 +51,23 @@ public class LinkageMonitor {
 
   public static void main(String[] arguments)
       throws RepositoryException, IOException, LinkageMonitorException {
-    if (arguments.length < 1) {
+    if (arguments.length < 1 || arguments[0].split(":").length != 2) {
       System.err.println(
-          "Please specify BOM coordinates. Example: com.google.cloud:libraries-bom:1.2.1");
+          "Please specify BOM coordinates without version. Example:"
+              + " com.google.cloud:libraries-bom");
       System.exit(1);
     }
     String bomCoordinates = arguments[0];
-    new LinkageMonitor().run(bomCoordinates);
+    List<String> coordinatesElements = Splitter.on(':').splitToList(bomCoordinates);
+
+    new LinkageMonitor().run(coordinatesElements.get(0), coordinatesElements.get(1));
   }
 
-  private void run(String bomCoordinates)
+  private void run(String groupId, String artifactId)
       throws RepositoryException, IOException, LinkageMonitorException {
-    Bom baseline = RepositoryUtility.readBom(bomCoordinates);
+    String latestBomCoordinates = findLatestCoordinates(groupId, artifactId);
+    System.out.println("BOM Coordinates: " + latestBomCoordinates);
+    Bom baseline = RepositoryUtility.readBom(latestBomCoordinates);
     ImmutableSet<SymbolProblem> problemsInBaseline =
         LinkageChecker.create(baseline).findSymbolProblems().keySet();
 
@@ -109,6 +118,16 @@ public class LinkageMonitor {
   static Optional<String> findSnapshotVersion(
       RepositorySystem repositorySystem, RepositorySystemSession session, Artifact artifact)
       throws VersionRangeResolutionException {
+    String version = findHighestVersion(repositorySystem, session, artifact);
+    if (version.contains("-SNAPSHOT")) {
+      return Optional.of(version);
+    }
+    return Optional.empty();
+  }
+
+  private static String findHighestVersion(
+      RepositorySystem repositorySystem, RepositorySystemSession session, Artifact artifact)
+      throws VersionRangeResolutionException {
     Artifact artifactWithVersionRange = artifact.setVersion("(0,]");
     VersionRangeRequest request =
         new VersionRangeRequest(
@@ -116,10 +135,19 @@ public class LinkageMonitor {
     VersionRangeResult versionResult = repositorySystem.resolveVersionRange(session, request);
 
     Verify.verify(versionResult.getHighestVersion() != null, "Highest version should not be null");
-    String version = versionResult.getHighestVersion().toString();
-    if (version.contains("-SNAPSHOT")) {
-      return Optional.of(version);
-    }
-    return Optional.empty();
+    return versionResult.getHighestVersion().toString();
+  }
+
+  private String findLatestCoordinates(String groupId, String artifactId)
+      throws VersionRangeResolutionException {
+    RepositorySystemSession session = RepositoryUtility.newSession(repositorySystem);
+    Artifact artifact = new DefaultArtifact(groupId, artifactId, null,
+        "0.0.1"); // dummy version
+    String highestVersion =
+        findHighestVersion(
+            repositorySystem,
+            session,
+            artifact);
+    return Artifacts.toCoordinates(artifact.setVersion(highestVersion));
   }
 }
