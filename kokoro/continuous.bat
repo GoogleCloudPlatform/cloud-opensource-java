c8021cf05e3bf910cb79b6cfab06d75efdea9eb0
Tomo Suzuki <suztomo@google.com>
2019-06-19 11:19:43 -0400

    reverted settings.xml

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -4,10 +4,6 @@ set JAVA_HOME=c:\program files\java\jdk1.8.0_152
 set PATH=%JAVA_HOME%\bin;%PATH%
 
 cd github/cloud-opensource-java
-
-mkdir %USERPROFILE%\.m2
-copy settings.xml %USERPROFILE%\.m2
-
 mvn -B clean install javadoc:jar
 
 exit /b %ERRORLEVEL%
