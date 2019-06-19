b332cc110271caaa41ce8b0510a5f7976b70c465
Tomo Suzuki <suztomo@google.com>
2019-06-19 09:20:43 -0400

    using home directory for settings.xml

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -6,5 +6,9 @@ set -e
 set -x
 
 cd github/cloud-opensource-java
+
 # M2_HOME is not used since Maven 3.5.0 https://maven.apache.org/docs/3.5.0/release-notes.html
-mvn -s settings.xml -B clean install javadoc:jar
+mkdir -p ${HOME}/.m2
+cp settings.xml ${HOME}/.m2
+
+mvn -B clean install javadoc:jar
