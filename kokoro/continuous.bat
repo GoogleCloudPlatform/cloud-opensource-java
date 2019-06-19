51ace88a30e13f36d20b295645dac316f92bf830
Tomo Suzuki <suztomo@google.com>
2019-06-19 09:15:52 -0400

    revert -X

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -4,6 +4,6 @@ set JAVA_HOME=c:\program files\java\jdk1.8.0_152
 set PATH=%JAVA_HOME%\bin;%PATH%
 
 cd github/cloud-opensource-java
-mvn -X -B clean install javadoc:jar
+mvn -B clean install javadoc:jar
 
 exit /b %ERRORLEVEL%
