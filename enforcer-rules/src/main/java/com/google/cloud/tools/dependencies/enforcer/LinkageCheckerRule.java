88e73bf3c31b3b2fdec10fe2a6d7134b5f91abcc
Tomo Suzuki <suztomo@google.com>
2019-07-11 15:06:01 -0400

    format

--- enforcer-rules/src/main/java/com/google/cloud/tools/dependencies/enforcer/LinkageCheckerRule.java
+++ enforcer-rules/src/main/java/com/google/cloud/tools/dependencies/enforcer/LinkageCheckerRule.java
@@ -133,12 +133,12 @@ public class LinkageCheckerRule extends AbstractNonCacheableEnforcerRule {
 
       // As sorted by level order, the first elements in classpath are the project and its direct
       // non-test dependencies.
-      int projectDependencyCount =
-          (int)
+      long projectDependencyCount =
+
               project.getDependencies().stream()
                   .filter(dependency -> !"test".equals(dependency.getScope()))
                   .count();
-      List<Path> entryPoints = classpath.subList(0, projectDependencyCount + 1);
+      List<Path> entryPoints = classpath.subList(0, (int) projectDependencyCount + 1);
 
       try {
 
