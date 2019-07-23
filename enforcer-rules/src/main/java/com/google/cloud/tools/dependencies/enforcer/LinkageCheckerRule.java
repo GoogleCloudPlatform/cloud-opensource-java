0ac78a5b10eb59d687344cab2dc499545da6637c
Tomo Suzuki <suztomo@google.com>
2019-07-23 15:03:57 -0400

    initial implementation

--- enforcer-rules/src/main/java/com/google/cloud/tools/dependencies/enforcer/LinkageCheckerRule.java
+++ enforcer-rules/src/main/java/com/google/cloud/tools/dependencies/enforcer/LinkageCheckerRule.java
@@ -28,6 +28,7 @@ import com.google.cloud.tools.opensource.classpath.SymbolProblem;
 import com.google.cloud.tools.opensource.dependencies.Artifacts;
 import com.google.cloud.tools.opensource.dependencies.NonTestDependencySelector;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSetMultimap;
@@ -35,8 +36,13 @@ import com.google.common.graph.Traverser;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Stack;
+import java.util.StringJoiner;
 import javax.annotation.Nonnull;
 import org.apache.maven.RepositoryUtils;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
@@ -59,6 +65,9 @@ import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.ArtifactTypeRegistry;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.graph.DependencyNode;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.transfer.ArtifactNotFoundException;
+import org.eclipse.aether.transfer.ArtifactTransferException;
 import org.eclipse.aether.util.graph.selector.AndDependencySelector;
 import org.eclipse.aether.util.graph.selector.ExclusionDependencySelector;
 
@@ -247,6 +256,7 @@ public class LinkageCheckerRule extends AbstractNonCacheableEnforcerRule {
     } catch (ComponentLookupException e) {
       throw new EnforcerRuleException("Unable to lookup a component " + e.getMessage(), e);
     } catch (DependencyResolutionException e) {
+
       throw new EnforcerRuleException(
           "Unable to build a dependency graph: " + e.getMessage(), e);
     }
@@ -268,6 +278,48 @@ public class LinkageCheckerRule extends AbstractNonCacheableEnforcerRule {
       return ClassPathBuilder.artifactsToClasspath(artifacts);
     } catch (RepositoryException ex) {
       throw new EnforcerRuleException("Failed to collect dependency " + ex.getMessage(), ex);
+    }
+  }
+
+  /**
+   * Returns message describing the path from the project root to a problematic artifact causing
+   * {@code exception}.
+   */
+  @VisibleForTesting
+  static String formatDependencyPath(DependencyResolutionException exception) {
+    Throwable cause = exception.getCause();
+    while (cause != null) {
+      if (cause instanceof ArtifactTransferException) {
+        ArtifactTransferException notFoundException = (ArtifactTransferException)cause;
+        Artifact problemArtifact = notFoundException.getArtifact();
+        return findPaths(
+            exception.getResult().getDependencyGraph(), problemArtifact);
+      } else {
+        cause = cause.getCause();
+      }
+    }
+    return null;
+  }
+
+  private static String findPaths(DependencyNode root, Artifact artifact) {
+    ImmutableList.Builder<ImmutableList<DependencyNode>> result = ImmutableList.builder();
+    findArtifact(result, root, new ArrayDeque<>(), artifact);
+    StringBuilder builder = new StringBuilder();
+    for (ImmutableList<DependencyNode> path: result.build()) {
+      builder.append(Joiner.on(">").join(path));
+      builder.append("\n");
+    }
+    return builder.toString();
+  }
+
+  private static void findArtifact(ImmutableList.Builder<ImmutableList<DependencyNode>> result, DependencyNode node, Deque<DependencyNode> path, Artifact artifact) {
+    if (node.getArtifact().equals(artifact)) {
+      result.add(ImmutableList.copyOf(path));
+    }
+    for (DependencyNode child : node.getChildren()) {
+      path.addLast(child);
+      findArtifact(result, child, path, artifact);
+      path.removeLast();
     }
   }
 }
