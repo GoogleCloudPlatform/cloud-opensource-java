ba4f2162e17db76278bde1613c4ca932542a98cf
Tomo Suzuki <suztomo@google.com>
2020-01-09 21:31:13 -0500

    One line after copyright

--- dependencies/src/main/java/com/google/cloud/tools/opensource/dependencies/ArtifactProblem.java
+++ dependencies/src/main/java/com/google/cloud/tools/opensource/dependencies/ArtifactProblem.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.google.cloud.tools.opensource.dependencies;
 
 import static com.google.common.base.Preconditions.checkNotNull;
