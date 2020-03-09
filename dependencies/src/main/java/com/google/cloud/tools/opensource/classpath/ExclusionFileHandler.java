a8ecd99ca2d744885c9acd14de5e5a86be4ec7ee
Tomo Suzuki <suztomo@google.com>
2020-03-09 16:38:47 -0400

    Parser for Linkage Checker exclusion file (#1269)

---
+++ dependencies/src/main/java/com/google/cloud/tools/opensource/classpath/ExclusionFileHandler.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.opensource.classpath;
+
+import com.google.common.collect.ImmutableList;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Handler for Linkage Checker exclusion XML files.
+ *
+ * <p>An instance of this class can process one document and cannot be reused for other documents.
+ */
+class ExclusionFileHandler extends DefaultHandler {
+
+  private ImmutableList.Builder<LinkageErrorMatcher> matchers;
+
+  /** LinkageError element that this handler is currently processing. */
+  private LinkageErrorMatcher linkageErrorMatcher;
+
+  /** Source or Target element that this handler is currently processing. */
+  private SymbolProblemMatcher symbolProblemMatcher;
+
+  ImmutableList<LinkageErrorMatcher> getMatchers() {
+    return matchers.build();
+  }
+
+  @Override
+  public void startDocument() {
+    if (matchers != null) {
+      throw new IllegalStateException("This handler started reading document already");
+    }
+    matchers = ImmutableList.builder();
+  }
+
+  @Override
+  public void startElement(
+      String namespaceUri, String localName, String qualifiedName, Attributes attributes)
+      throws SAXException {
+    if (!namespaceUri.isEmpty()) {
+      throw new SAXException(
+          "unrecognized element: " + qualifiedName + " in namespace " + namespaceUri);
+    }
+    switch (localName) {
+      case "LinkageCheckerFilter":
+        break;
+      case "LinkageError":
+        linkageErrorMatcher = new LinkageErrorMatcher();
+        matchers.add(linkageErrorMatcher);
+        break;
+      case "Source":
+        SourceMatcher sourceMatcher = new SourceMatcher();
+        linkageErrorMatcher.setSourceMatcher(sourceMatcher);
+        symbolProblemMatcher = sourceMatcher;
+        break;
+      case "Target":
+        TargetMatcher targetMatcher = new TargetMatcher();
+        linkageErrorMatcher.setTargetMatcher(targetMatcher);
+        symbolProblemMatcher = targetMatcher;
+        break;
+      case "Package":
+        symbolProblemMatcher.addChild(new PackageMatcher(attributes.getValue("name")));
+        break;
+      case "Class":
+        String classNameOnClass = attributes.getValue("name");
+        symbolProblemMatcher.addChild(new ClassMatcher(classNameOnClass));
+        break;
+      case "Method":
+        String classNameOnMethod = attributes.getValue("className");
+        MethodMatcher methodMatcher =
+            new MethodMatcher(classNameOnMethod, attributes.getValue("name"));
+        symbolProblemMatcher.addChild(methodMatcher);
+        break;
+      case "Field":
+        String classNameOnField = attributes.getValue("className");
+        FieldMatcher fieldMatcher = new FieldMatcher(classNameOnField, attributes.getValue("name"));
+        symbolProblemMatcher.addChild(fieldMatcher);
+        break;
+      default:
+        throw new SAXException("Unknown tag " + localName);
+    }
+  }
+}
---
+++ dependencies/src/main/java/com/google/cloud/tools/opensource/classpath/ExclusionFileParser.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.opensource.classpath;
+
+import com.google.common.collect.ImmutableList;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+/**
+ * Parser for Linkage Checker exclusion files.
+ *
+ * <p>The exclusion file for Linkage Checker is an XML file. Its top-level element is
+ * LinkageCheckerFilter. The XML file contains the following structure:
+ *
+ * <ul>
+ *   <li>A LinkageCheckerFilter element has zero or more LinkageError elements.
+ *   <li>A LinkageError element has at least one of Target element and Source element.
+ *   <li>A Target element has Package, Class, Method, and Field elements. A Source element has
+ *       Artifact, Package, and Class elements.
+ *   <li>Method and Field elements have “className” attribute.
+ * </ul>
+ *
+ * <p>Each type of the element works as a corresponding matcher, such as LinkageErrorMatcher for a
+ * LinkageError element and SourceMatcher for Source element. Given a linkage error, they work as
+ * below:
+ *
+ * <ul>
+ *   <li>A LinkageErrorMatcher matches when all of its child elements match the linkage error.
+ *   <li>A SourceMatcher matches a linkage error when the source class of the error matches one of
+ *       its child elements.
+ *   <li>A TargetMatcher matches a linkage error when the target symbol (class, method, or field) of
+ *       the error matches one of its child elements.
+ *   <li>A PackageMatcher matches the classes that have Java package specified by its name field.
+ *       Prefix to specify child packages.
+ *   <li>A ClassMatcher matches the class specified by its name attribute. ArtifactMatcher,
+ *       PackageMatcher, and ClassMatcher also match methods and fields on their matching classes.
+ *   <li>A MethodMatcher matches method symbol specified by className and name attribute.
+ *   <li>A FieldMatcher matches field symbol specified by className and name attribute.
+ * </ul>
+ */
+class ExclusionFileParser {
+
+  static ImmutableList<LinkageErrorMatcher> parse(Path exclusionFile)
+      throws SAXException, IOException {
+
+    XMLReader xmlReader = XMLReaderFactory.createXMLReader();
+    ExclusionFileHandler handler = new ExclusionFileHandler();
+    xmlReader.setContentHandler(handler);
+
+    InputSource inputSource = new InputSource(new FileInputStream(exclusionFile.toFile()));
+    xmlReader.parse(inputSource);
+    return handler.getMatchers();
+  }
+}
