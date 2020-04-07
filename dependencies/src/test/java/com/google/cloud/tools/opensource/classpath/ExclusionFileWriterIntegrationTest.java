34bc1445010fe626cef8a7680b88b7dcf7768e7d
Tomo Suzuki <suztomo@google.com>
2020-04-07 11:34:13 -0400

    Integration test

--- dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/ExclusionFileWriterIntegrationTest.java
+++ dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/ExclusionFileWriterIntegrationTest.java
@@ -17,22 +17,52 @@
 package com.google.cloud.tools.opensource.classpath;
 
 
+import com.google.cloud.tools.opensource.dependencies.Artifacts;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSetMultimap;
 import com.google.common.truth.Truth;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.transform.TransformerException;
 import org.eclipse.aether.RepositoryException;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ExclusionFileWriterIntegrationTest {
 
-  @Test
-  public void testExclusion() throws IOException, RepositoryException {
-    Path exclusionFile = null;
-    LinkageCheckerMain.main(
-        new String[] {"-a", "org.apache.beam:xyz:2.20.0", "-w", exclusionFile.toString()});
+  Path exclusionFile;
 
-    LinkageChecker linkagechecker = null; // with exclusionFile
+  @Before
+  public void setup() throws IOException {
+    // exclusionFile = Files.createTempFile("output", ".xml");
+
+    exclusionFile = Paths.get("/tmp/output.xml").toAbsolutePath();
+
+    // exclusionFile.toFile().deleteOnExit();
+  }
+
+  @Test
+  public void testExclusion()
+      throws IOException, RepositoryException, TransformerException, XMLStreamException {
+
+    Artifact artifact = new DefaultArtifact("org.apache.beam:beam-runners-google-cloud-dataflow-java:2.19.0");
+    LinkageCheckerMain.main(
+        new String[] {"-a", Artifacts.toCoordinates(artifact), "-w", exclusionFile.toString()});
+
+    System.out.println("Wrote exclusion file at " + exclusionFile);
+
+    ClassPathBuilder classPathBuilder = new ClassPathBuilder();
+    ClassPathResult classPathResult = classPathBuilder.resolve(ImmutableList.of(artifact));
+
+    LinkageChecker linkagechecker = LinkageChecker.create(
+        classPathResult.getClassPath(),
+        ImmutableList.of(),
+        exclusionFile); // with exclusionFile
 
     ImmutableSetMultimap<SymbolProblem, ClassFile> symbolProblems =
         linkagechecker.findSymbolProblems();
