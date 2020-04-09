4c0a7e0ba9c69967485277f9aca30c39ec8b91d5
Tomo Suzuki <suztomo@google.com>
2020-04-09 10:38:09 -0400

    Test that fails

---
+++ dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/LinkageCheckerMainIntegrationTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.opensource.classpath;
+
+import static com.google.cloud.tools.opensource.classpath.TestHelper.COORDINATES;
+import static com.google.cloud.tools.opensource.classpath.TestHelper.absolutePathOfResource;
+import static com.google.cloud.tools.opensource.classpath.TestHelper.classPathEntryOfResource;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import com.google.cloud.tools.opensource.dependencies.DependencyGraph;
+import com.google.cloud.tools.opensource.dependencies.DependencyGraphBuilder;
+import com.google.cloud.tools.opensource.dependencies.UnresolvableArtifactProblem;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSetMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.truth.Correspondence;
+import com.google.common.truth.Truth;
+import com.google.common.truth.Truth8;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.apache.commons.cli.ParseException;
+import org.eclipse.aether.RepositoryException;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.graph.Dependency;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LinkageCheckerMainIntegrationTest {
+
+  @Test
+  public void testJarFiles() throws IOException, URISyntaxException, RepositoryException {
+
+    Path googleCloudCore = absolutePathOfResource("testdata/google-cloud-core-1.48.0.jar");
+    Path googleCloudFirestore = absolutePathOfResource("testdata/google-cloud-firestore-0.65.0-beta.jar");
+
+    String jarArgument = googleCloudCore + "," + googleCloudFirestore;
+    LinkageCheckerMain.main(new String[] {
+        "-j", jarArgument
+    });
+  }
+}
