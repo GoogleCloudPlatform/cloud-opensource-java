792502536ce5c58bf2e5641bf0afa3ee0938c120
Tomo Suzuki <suztomo@google.com>
2020-04-09 10:47:44 -0400

    Fixed the problem

--- dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/LinkageCheckerMainIntegrationTest.java
+++ dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/LinkageCheckerMainIntegrationTest.java
@@ -16,52 +16,55 @@
 
 package com.google.cloud.tools.opensource.classpath;
 
-import static com.google.cloud.tools.opensource.classpath.TestHelper.COORDINATES;
 import static com.google.cloud.tools.opensource.classpath.TestHelper.absolutePathOfResource;
-import static com.google.cloud.tools.opensource.classpath.TestHelper.classPathEntryOfResource;
-import static com.google.common.collect.ImmutableList.toImmutableList;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
 
-import com.google.cloud.tools.opensource.dependencies.DependencyGraph;
-import com.google.cloud.tools.opensource.dependencies.DependencyGraphBuilder;
-import com.google.cloud.tools.opensource.dependencies.UnresolvableArtifactProblem;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.ImmutableSetMultimap;
-import com.google.common.collect.Lists;
-import com.google.common.truth.Correspondence;
 import com.google.common.truth.Truth;
-import com.google.common.truth.Truth8;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import org.apache.commons.cli.ParseException;
 import org.eclipse.aether.RepositoryException;
-import org.eclipse.aether.artifact.Artifact;
-import org.eclipse.aether.artifact.DefaultArtifact;
-import org.eclipse.aether.graph.Dependency;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class LinkageCheckerMainIntegrationTest {
 
+  private final ByteArrayOutputStream capturedOutputStream = new ByteArrayOutputStream();
+  private final PrintStream originalStandardOut = System.out;
+
+  @Before
+  public void setup() {
+    System.setOut(new PrintStream(capturedOutputStream));
+  }
+
+  @After
+  public void restoreStreams() {
+    System.setOut(originalStandardOut);
+  }
+
   @Test
   public void testJarFiles() throws IOException, URISyntaxException, RepositoryException {
 
     Path googleCloudCore = absolutePathOfResource("testdata/google-cloud-core-1.48.0.jar");
-    Path googleCloudFirestore = absolutePathOfResource("testdata/google-cloud-firestore-0.65.0-beta.jar");
+    Path googleCloudFirestore =
+        absolutePathOfResource("testdata/google-cloud-firestore-0.65.0-beta.jar");
+    Path guava = absolutePathOfResource("testdata/guava-23.5-jre.jar");
 
-    String jarArgument = googleCloudCore + "," + googleCloudFirestore;
-    LinkageCheckerMain.main(new String[] {
-        "-j", jarArgument
-    });
+    String jarArgument = googleCloudCore + "," + googleCloudFirestore + "," + guava;
+
+    // This should not raise IOException
+    LinkageCheckerMain.main(new String[] {"-j", jarArgument});
+
+    String output = new String(capturedOutputStream.toByteArray());
+
+    // Gax is not in the JAR list
+    Truth.assertThat(output)
+        .contains(
+            "Class com.google.api.gax.retrying.ResultRetryAlgorithm is not found;\n"
+                + "  referenced by 1 class file\n"
+                + "    com.google.cloud.ExceptionHandler (/Users/suztomo/cloud-opensource-java"
+                + "/dependencies/target/test-classes/testdata/google-cloud-core-1.48.0.jar)");
   }
 }
