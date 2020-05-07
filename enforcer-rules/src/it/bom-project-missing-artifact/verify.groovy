970afd11beaef52bcb2d73d920e0a0d0520d6a34
Tomo Suzuki <suztomo@google.com>
2020-05-07 11:56:28 -0400

    Test to confirm the failure behavior

--- enforcer-rules/src/it/bom-project-missing-artifact/verify.groovy
+++ enforcer-rules/src/it/bom-project-missing-artifact/verify.groovy
@@ -1,4 +1,5 @@
 def buildLog = new File(basedir, "build.log").text
 
-assert buildLog.contains("No reachable error found")
-assert buildLog.contains("xerces:xerces-impl:2.6.2")
+assert buildLog.contains("Failed to collect dependency: "
+        +"[xerces:xerces-impl:jar:2.6.2 was not resolved. "
+        +"Dependency path: ant:ant:jar:1.6.2 (compile) > xerces:xerces-impl:jar:2.6.2 (compile?)")
