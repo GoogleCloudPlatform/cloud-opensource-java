e821c47c4995296a5ed9986ec2958105760f6a0a
Elliotte Rusty Harold <elharo@ibiblio.org>
2020-06-04 11:26:32 -0400

    fix integration test

--- enforcer-rules/src/it/bom-project-missing-artifact/verify.groovy
+++ enforcer-rules/src/it/bom-project-missing-artifact/verify.groovy
@@ -5,4 +5,4 @@ assert buildLog.contains(
         "[xerces:xerces-impl:jar:2.6.2 was not resolved. "
         +"Dependency path: ant:ant:jar:1.6.2 (compile) > xerces:xerces-impl:jar:2.6.2 (compile?)")
 assert buildLog.contains(        
-        "xml-apis:xml-apis:jar:2.6.2 was not resolved. Dependency path: " + "ant:ant:jar:1.6.2 (compile) > xml-apis:xml-apis:jar:2.6.2 (compile?)"
+        "xml-apis:xml-apis:jar:2.6.2 was not resolved. Dependency path: " + "ant:ant:jar:1.6.2 (compile) > xml-apis:xml-apis:jar:2.6.2 (compile?)")
