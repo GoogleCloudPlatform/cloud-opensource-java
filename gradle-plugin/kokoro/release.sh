44c22f2896c92295ee3402f00b24001de4fe5471
Tomo Suzuki <suztomo@google.com>
2020-07-06 11:07:24 -0400

    gradle build when releasing

--- gradle-plugin/kokoro/release.sh
+++ gradle-plugin/kokoro/release.sh
@@ -10,7 +10,7 @@ readonly PUBLISH_SECRET=$(cat "${KOKORO_KEYSTORE_DIR}/72743_gradle_publish_secre
 
 cd github/cloud-opensource-java/gradle-plugin
 
-./gradlew check publishPlugins \
+./gradlew build publishPlugins \
   -Pgradle.publish.key="${PUBLISH_KEY}" \
   -Pgradle.publish.secret="${PUBLISH_SECRET}" \
   --info --stacktrace
