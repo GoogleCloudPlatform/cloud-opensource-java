dd53bf1b28aea4f367f8b34aad7a2033c80539e2
Tomo Suzuki <suztomo@google.com>
2020-07-07 13:56:33 -0400

    ise check link

--- gradle-plugin/kokoro/release.sh
+++ gradle-plugin/kokoro/release.sh
@@ -6,8 +6,9 @@ set -o xtrace
 mkdir -p $HOME/.gradle
 readonly HOME_GRADLE_PROPERTY="$HOME/.gradle/gradle.properties"
 
-# Recommended way to store API key
+# Recommended way to store API key in
 # https://guides.gradle.org/publishing-plugins-to-gradle-plugin-portal/
+# and go/yaqs/5342701566951424 for ISE check.
 echo -n 'gradle.publish.key=' >> $HOME_GRADLE_PROPERTY
 cat "${KOKORO_KEYSTORE_DIR}/72743_gradle_publish_key" >> $HOME_GRADLE_PROPERTY
 echo -n 'gradle.publish.secret=' >> $HOME_GRADLE_PROPERTY
