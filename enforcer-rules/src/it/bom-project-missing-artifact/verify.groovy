bb0acefc42b7fcbf4dd34d8ce813167ac138f1f7
Elliotte Rusty Harold <elharo@ibiblio.org>
2020-07-14 15:15:49 -0400

    errors now hidden by not following optional

--- enforcer-rules/src/it/bom-project-missing-artifact/verify.groovy
+++ enforcer-rules/src/it/bom-project-missing-artifact/verify.groovy
@@ -1,7 +1,1 @@
 def buildLog = new File(basedir, "build.log").text
-
-assert buildLog.contains("Failed to collect dependency: ")
-assert buildLog.contains(
-        "xerces:xerces-impl:jar:2.6.2 was not resolved. Dependency path: ant:ant:jar:1.6.2 (compile) > xerces:xerces-impl:jar:2.6.2 (compile?)")
-assert buildLog.contains(        
-        "xml-apis:xml-apis:jar:2.6.2 was not resolved. Dependency path: ant:ant:jar:1.6.2 (compile) > xml-apis:xml-apis:jar:2.6.2 (compile?)")
