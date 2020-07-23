e00e9b73c027b3623389f7088d54ec5a78824bf3
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2020-07-23 10:29:07 +0000

    drop optional dependencies when running enforcer rule (#1530)
    * drop optional dependencies when running enforcer rule

--- enforcer-rules/src/it/bom-project-error/verify.groovy
+++ enforcer-rules/src/it/bom-project-error/verify.groovy
@@ -1,7 +1,16 @@
 def buildLog = new File(basedir, "build.log").text.replaceAll("\\r\\n", "\n")
 
 assert buildLog.contains('''\
-[ERROR] Linkage Checker rule found 1 error. Linkage error report:
+[ERROR] Linkage Checker rule found 4 errors. Linkage error report:
+Class org.apache.avalon.framework.logger.Logger is not found;
+  referenced by 1 class file
+    org.apache.commons.logging.impl.AvalonLogger (commons-logging:commons-logging:1.1.1)
+Class org.apache.log.Hierarchy is not found;
+  referenced by 1 class file
+    org.apache.commons.logging.impl.LogKitLogger (commons-logging:commons-logging:1.1.1)
+Class org.apache.log.Logger is not found;
+  referenced by 1 class file
+    org.apache.commons.logging.impl.LogKitLogger (commons-logging:commons-logging:1.1.1)
 (com.google.guava:guava:20.0) com.google.common.base.Verify's method verify(boolean arg1, String arg2, Object arg3) is not found;
   referenced by 3 class files
     io.grpc.internal.ServiceConfigInterceptor (io.grpc:grpc-core:1.17.1)
@@ -10,6 +19,9 @@ assert buildLog.contains('''\
 ''')
 
 assert buildLog.contains('''[ERROR] Problematic artifacts in the dependency tree:
+commons-logging:commons-logging:1.1.1 is at:
+  com.google.api-client:google-api-client:1.27.0 (compile) / com.google.oauth-client:google-oauth-client:1.27.0 (compile) / com.google.http-client:google-http-client:1.27.0 (compile) / com.google.android:android:1.5_r4 (provided) / commons-logging:commons-logging:1.1.1 (compile)
+  and 3 other dependency paths.
 com.google.guava:guava:20.0 is at:
   com.google.api-client:google-api-client:1.27.0 (compile) / com.google.guava:guava:20.0 (compile)
   and 3 other dependency paths.
