57d7e53215d94d5a298428986007eac3a449c3f5
Tomo Suzuki <suztomo@google.com>
2020-08-07 15:13:36 -0400

    systemProp prefix

--- gradle-plugin/kokoro/release.sh
+++ gradle-plugin/kokoro/release.sh
@@ -10,9 +10,9 @@ export GRADLE_USER_HOME=$HOME/.gradle
 # Recommended way to store API key in
 # https://guides.gradle.org/publishing-plugins-to-gradle-plugin-portal/
 # and go/yaqs/5342701566951424 for ISE check.
-echo -n 'gradle.publish.key=' >> $HOME_GRADLE_PROPERTY
+echo -n 'systemProp.gradle.publish.key=' >> $HOME_GRADLE_PROPERTY
 cat "${KOKORO_KEYSTORE_DIR}/72743_gradle_publish_key" >> $HOME_GRADLE_PROPERTY
-echo -n 'gradle.publish.secret=' >> $HOME_GRADLE_PROPERTY
+echo -n 'systemProp.gradle.publish.secret=' >> $HOME_GRADLE_PROPERTY
 cat "${KOKORO_KEYSTORE_DIR}/72743_gradle_publish_secret" >> $HOME_GRADLE_PROPERTY
 
 
