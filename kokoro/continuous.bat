a63028d14321f1eb2a9d07e8b6e03f49a7661784
Tomo Suzuki <suztomo@google.com>
2020-08-24 11:16:52 -0400

    scan

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -14,6 +14,6 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 @echo on
 
 cd gradle-plugin
-call gradlew.bat build publishToMavenLocal
+call gradlew.bat build publishToMavenLocal --scan
 
 exit /b %ERRORLEVEL%
