cb63896c9d4722b6b73a46b4f6ca86ee33ac2845
Tomo Suzuki <suztomo@google.com>
2021-04-05 17:49:55 -0400

    Upgrading dependencies of this project (#2012)
    * Dependency upgrades
    
    * ClassPathEntryTest.resolveArtifact specifies Maven Central
    
    * No static import for CENTRAL

--- dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/ClassPathEntryTest.java
+++ dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/ClassPathEntryTest.java
@@ -19,6 +19,7 @@ package com.google.cloud.tools.opensource.classpath;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
+
 import com.google.cloud.tools.opensource.dependencies.RepositoryUtility;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.testing.EqualsTester;
@@ -143,6 +144,7 @@ public class ClassPathEntryTest {
 
     Artifact artifact = new DefaultArtifact(coordinates);    
     ArtifactRequest artifactRequest = new ArtifactRequest();
+    artifactRequest.addRepository(RepositoryUtility.CENTRAL);
     artifactRequest.setArtifact(artifact);
     ArtifactResult artifactResult = system.resolveArtifact(session, artifactRequest);
     
