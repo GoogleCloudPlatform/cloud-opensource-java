ecb2270e90d5d56decfe04c55bf59c7194bc946a
Tomo Suzuki <suztomo@google.com>
2021-11-15 23:04:29 -0500

    Added a check for no-downgrade rule for BOMs (#2237)
    * Added a check for no-downgrade rule
    
    * Updated comment
    
    * Fixed the versions in the BOM
    
    * Applied review

--- dashboard/src/test/java/com/google/cloud/tools/opensource/dashboard/BomTest.java
+++ dashboard/src/test/java/com/google/cloud/tools/opensource/dashboard/BomTest.java
@@ -16,40 +16,52 @@
 
 package com.google.cloud.tools.opensource.dashboard;
 
+import com.google.cloud.tools.opensource.classpath.ClassPathBuilder;
+import com.google.cloud.tools.opensource.classpath.ClassPathEntry;
+import com.google.cloud.tools.opensource.classpath.ClassPathResult;
+import com.google.cloud.tools.opensource.classpath.DependencyMediation;
+import com.google.cloud.tools.opensource.dependencies.Artifacts;
+import com.google.cloud.tools.opensource.dependencies.Bom;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 
 import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.util.version.GenericVersionScheme;
+import org.eclipse.aether.version.InvalidVersionSpecificationException;
+import org.eclipse.aether.version.Version;
+import org.eclipse.aether.version.VersionScheme;
 import org.junit.Assert;
 import org.junit.Test;
-import com.google.cloud.tools.opensource.dependencies.Bom;
-import com.google.cloud.tools.opensource.dependencies.MavenRepositoryException;
 
 public class BomTest {
-  
+  private static VersionScheme versionScheme = new GenericVersionScheme();
+
   @Test
-  public void testLtsBom()
-      throws IOException, MavenRepositoryException {
+  public void testLtsBom() throws Exception {
     Path bomPath = Paths.get("..", "boms", "cloud-lts-bom", "pom.xml").toAbsolutePath();  
     checkBom(bomPath);
   }
-  
+
   @Test
-  public void testLibrariesBom()
-      throws IOException, MavenRepositoryException {
+  public void testLibrariesBom() throws Exception {
     Path bomPath = Paths.get("..", "boms", "cloud-oss-bom", "pom.xml").toAbsolutePath();  
     checkBom(bomPath);
   }
 
-  private void checkBom(Path bomPath) throws MavenRepositoryException, IOException {
-    List<Artifact> artifacts = Bom.readBom(bomPath).getManagedDependencies();
+  private void checkBom(Path bomPath) throws Exception {
+    Bom bom = Bom.readBom(bomPath);
+    List<Artifact> artifacts = bom.getManagedDependencies();
     for (Artifact artifact : artifacts) {
       assertReachable(buildMavenCentralUrl(artifact));
     }
+
+    assertNoDowngradeRule(bom);
   }
 
   private static String buildMavenCentralUrl(Artifact artifact) {
@@ -71,5 +83,74 @@ public class BomTest {
     } catch (IOException ex) {
       Assert.fail("Could not reach " + url + "\n" + ex.getMessage());
     }
+  }
+
+  /**
+   * Asserts that the members of the {@code bom} satisfy the no-downgrade rule. This rule means that
+   * the members have the highest versions among the dependencies of them. If there's a violation,
+   * users of the BOM would see our BOM downgrading certain dependencies. Downgrading a dependency
+   * is bad practice in general because newer versions have more features (classes and methods).
+   *
+   * <p>For example, if google-http-client 1.40.1 is in the BOM, then no other libraries in the BOM
+   * depend on the higher version of the google-http-client.
+   *
+   * @param bom the BOM to assert with this no-downgrade rule.
+   */
+  private static void assertNoDowngradeRule(Bom bom) throws InvalidVersionSpecificationException {
+    List<String> violations = new ArrayList<>();
+    Map<String, Artifact> bomArtifacts = new HashMap<>();
+    for (Artifact artifact : bom.getManagedDependencies()) {
+      bomArtifacts.put(Artifacts.makeKey(artifact), artifact);
+    }
+
+    for (Artifact artifact : bom.getManagedDependencies()) {
+      violations.addAll(findNoDowngradeViolation(bomArtifacts, artifact));
+    }
+
+    String violationMessage = Joiner.on("\n").join(violations);
+    Assert.assertTrue(violationMessage, violations.isEmpty());
+  }
+
+  /**
+   * Returns messages describing the violation of the no-downgrade rule by {@code artifact} against
+   * the BOM containing {@code bomArtifacts}. An empty list if there is no violations.
+   */
+  private static ImmutableList<String> findNoDowngradeViolation(
+      Map<String, Artifact> bomArtifacts, Artifact artifact)
+      throws InvalidVersionSpecificationException {
+    ImmutableList.Builder<String> violations = ImmutableList.builder();
+
+    ClassPathBuilder classPathBuilder = new ClassPathBuilder();
+    ClassPathResult result =
+        classPathBuilder.resolve(ImmutableList.of(artifact), false, DependencyMediation.MAVEN);
+    for (ClassPathEntry entry : result.getClassPath()) {
+      Artifact transitiveDependency = entry.getArtifact();
+      String key = Artifacts.makeKey(transitiveDependency);
+      Artifact bomArtifact = bomArtifacts.get(key);
+      if (bomArtifact == null) {
+        // transitiveDependency is not part of the BOM
+        continue;
+      }
+
+      Version versionInBom = versionScheme.parseVersion(bomArtifact.getVersion());
+      Version versionInTransitiveDependency =
+          versionScheme.parseVersion(transitiveDependency.getVersion());
+
+      if (versionInTransitiveDependency.compareTo(versionInBom) <= 0) {
+        // When versionInTransitiveDependency is less than or equal to versionInBom, it satisfies
+        // the no-downgrade rule.
+        continue;
+      }
+
+      // A violation of the no-downgrade rule is found.
+      violations.add(
+          artifact
+              + " has a transitive dependency "
+              + transitiveDependency
+              + ". This is higher version than "
+              + bomArtifact
+              + " in the BOM");
+    }
+    return violations.build();
   }
 }
