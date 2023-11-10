605b80882b70845a0e932be7b4ae6c900394512a
Burke Davison <burkedavison@google.com>
2023-11-10 13:13:43 -0500

    chore: remove unused import

--- dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/LinkageCheckerMainIntegrationTest.java
+++ dependencies/src/test/java/com/google/cloud/tools/opensource/classpath/LinkageCheckerMainIntegrationTest.java
@@ -34,7 +34,6 @@ import org.hamcrest.core.StringStartsWith;
 import org.junit.After;
 import org.junit.Assume;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class LinkageCheckerMainIntegrationTest {
